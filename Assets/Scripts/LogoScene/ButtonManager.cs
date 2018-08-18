52a884182c5890c334570b7aeecd2af1fde0af6b
Kim Hyeonwoo <1reds@naver.com>
2018-08-19 04:39:48 +0900

    Add Logo Scene
    1. 시작 화면 추가
    2. 엔딩 화면 추가

---
+++ Assets/Scripts/LogoScene/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LogoScene
+{
+	public class ButtonManager : MonoBehaviour 
+	{
+		public void OnGameStartBtnClick()
+		{
+			SceneManager.LoadScene("Main");
+		}
+
+		public void OnOptionBtnClick()
+		{
+			// TODO: 옵션창 띄울 것!
+		}
+	}
+}
